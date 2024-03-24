a609061b5046350a63ad10a7c7c86b289a38d179
Santiya Georgieva <santiya.georgigeva@gmail.com>
2024-03-24 23:59:52 +0200

    update the controllers to use the dynamic properties for the calculations

--- controllers/truckController.js
+++ controllers/truckController.js
@@ -1,14 +1,15 @@
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const sendEmail = require('../utils/emailOffer');
-const pool = require('../utils/pool');
-const pc = require('picocolors');
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const sendEmail = require("../utils/emailOffer");
+const sql = require("../utils/sql");
 
-exports.truckPriceOffer = catchAsync(async (req, res, next) => {
-  // console.log('req.body', req.body);
+exports.truckGondnolaPriceOffer = catchAsync(async (req, res, next) => {
   try {
-    if (Object.keys(req.body).length === 0 || Object.keys(req.body.values).length === 0) {
-      return res.status(400).send('Empty object provided');
+    if (
+      Object.keys(req.body).length === 0 ||
+      Object.keys(req.body.values).length === 0
+    ) {
+      next(new AppError("Empty object provided"), 400);
     }
 
     const {
@@ -26,155 +27,155 @@ exports.truckPriceOffer = catchAsync(async (req, res, next) => {
       fitting_right,
       assembly
     } = req?.body?.values;
-    const { title } = req?.body;
-    var finalPrice;
-    let l = Number(length) + 0.6;
 
-    if (title === 'card_text4') {
-      let totalLength = 0;
-      let priceTarpaulin = 0;
-      const longitudinalPocketPrice = 0.3;
-      const fittingPrice = 50;
-      const assemblyPrice = 1.2;
-      let totalWidth = 0;
+    const prices = await sql.allTruckGondolaPrices();
 
+    if (Object.keys(prices).length > 0) { 
+      const {
+        longitudinal_pocket_price,
+        fitting_price,
+        assembly_price,
+        tarpaulin_price_1,
+        tarpaulin_price_2,
+      } = prices[0];
+      const priceTarpaulin = tarpaulin_type === "680гр/кв.м" ? tarpaulin_price_1 : tarpaulin_price_2;
+      let l = Number(length) + 0.6;
       let w = Number(width) + 0.8;
       const hood_value = Number(hood);
       const back_cover_value = Number(back_cover);
       const falling_pipe_value = Number(falling_pipe);
       const falling_right_value = Number(falling_right);
-      const number_stretches_value = (Number(number_stretches) * 0.2);
-
-      totalWidth = (w + hood_value + back_cover_value + number_stretches_value).toFixed(2);
-      totalLength = (l + falling_pipe_value + falling_right_value + number_stretches_value).toFixed(2);
-
-      if (longitudinal_pocket) {
-        totalLength = (Number(totalLength) + Number(longitudinalPocketPrice)).toFixed(2);
-      }
-
-      if (tarpaulin_type === '680гр/кв.м') {
-        priceTarpaulin = 12;
-      } else {
-        priceTarpaulin = 16;
-      }
-
-      finalPrice = (Number(totalWidth) * Number(totalLength) * Number(priceTarpaulin)).toFixed(2);
-
-      if (fitting_right || fitting_left) {
-        finalPrice = (Number(finalPrice) + Number(fittingPrice)).toFixed(2);
-      }
-
-      if (assembly) {
-        finalPrice = (Number(finalPrice) * Number(assemblyPrice)).toFixed(2);
-      }
-    } else {
+      const number_stretches_value = Number(number_stretches) * 0.2;
+      let totalWidth = 0;
       let totalLength = 0;
-      let h = 3;
-      totalLength = l * h * 15;
+      var finalPrice;
 
-      if (title === 'card_text8') {
-        finalPrice = (totalLength + 50).toFixed(2);
-      } else if (title === 'card_text7') {
-        finalPrice = (totalLength + 25).toFixed(2);
+      totalWidth = (
+        w +
+        hood_value +
+        back_cover_value +
+        number_stretches_value
+      ).toFixed(2);
+      totalLength = (
+        l +
+        falling_pipe_value +
+        falling_right_value +
+        number_stretches_value
+      ).toFixed(2);
+  
+      if (longitudinal_pocket) {
+        totalLength = (
+          Number(totalLength) + Number(longitudinal_pocket_price)
+        ).toFixed(2);
       }
-    }
-    res.status(200).json({
-      'status': 'success',
-      'result': finalPrice
-    });
+  
+      finalPrice = (
+        Number(totalWidth) *
+        Number(totalLength) *
+        Number(priceTarpaulin)
+      ).toFixed(2);
+  
+      if (fitting_right || fitting_left) {
+        finalPrice = (Number(finalPrice) + Number(fitting_price)).toFixed(2);
+      }
+  
+      if (assembly) {
+        finalPrice = (Number(finalPrice) * Number(assembly_price)).toFixed(2);
+      }
+  
+      res.status(200).json({
+        status: "success",
+        result: finalPrice
+      });
+    } 
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error
-    });
+    return next(new AppError(error), 400);
   }
 });
 
-exports.truckOfferFile = catchAsync(async(req, res, next) => {
+exports.truckShutterPriceOffer = catchAsync(async (req, res, next) => {
   try {
-    if (Object.keys(req.body).length === 0) {
-      return res.status(400).send('Empty object provided');
+    if (
+      Object.keys(req.body).length === 0 ||
+      Object.keys(req.body.values).length === 0
+    ) {
+      next(new AppError("Empty object provided"), 400);
     }
 
-    let response = await pool.query("INSERT INTO `truck_covers` SET ?", req.body, function (error, results, fields) {
-      pool.release();
-      return results;
+    const { title } = req?.body;
+    const { length } = req.body.values;
+    const l = Number(length) + 0.6;
+    const h = 3;
+    const totalLength = l * h * 15;
+    var finalPrice;
+    let price;
+
+    if (title === "card_text8") { 
+      price = await sql.allTruckWithoutShutterPrices();
+      finalPrice = (totalLength + price[0].without_shutters_price).toFixed(2);
+    } else {
+      price = await sql.allTruckWithShutterPrices();
+      finalPrice = (totalLength + price[0].with_shutter_price).toFixed(2);
+    }
+    
+    res.status(200).json({
+      status: "success",
+      result: finalPrice
     });
-
-    // function main(sql) {
-    //   return new Promise((resolve, reject) => {
-    //       pool.getConnection((err, connection) => {
-    //       if (err) throw err;
-    //         connection.query(sql, (errt, rows) => {
-    //         connection.release(); // return the connection to pool
-    //         if (errt) throw err;
-    //         // console.log(rows);
-    //         resolve(rows);
-    //       });
-    //     });
-    //   });
-    // }
-
-    // const response = async function queries() {
-    //   let Q = "INSERT INTO `truck_covers` SET ?";
-    //   let result = await main(Q);
-    //   return result;
-    // }
-
-  return res.status(200).json({
-    success: true,
-    status: "success",
-    message: "File information saved sucessfully!",
-    offerId: response[0].insertId
-  });
   } catch (error) {
-    console.error('error', error);
-    return res.status(400).json({
-      success: false,
-      message: error
+    return next(new AppError(error), 400);
+  }
+});
+
+exports.truckOfferFile = catchAsync(async (req, res, next) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      next(new AppError("Empty object provided"), 400);
+    }
+
+    const response = await sql.truckOfferSaveFile(req.body);
+
+    return res.status(200).json({
+      success: true,
+      status: "success",
+      message: "File information saved sucessfully!",
+      offerId: response
     });
+  } catch (error) {
+    console.error("error", error);
+    return next(new AppError(error), 500);
   }
 });
 
 exports.truckOfferComparedFiles = catchAsync(async (req, res, next) => {
-  // console.log('req', req.body);
   try {
     const newData = req.body;
 
     if (!newData.id || !newData.filename || !newData.type || !newData.size) {
-      return res.status(400).send('Missing required parameters');
+      return next(new AppError("Missing required parameters"), 400);
     }
 
-    let response = await poolObject.pool.query('UPDATE truck_covers SET filename = ?, type = ?, size = ? WHERE id = ?', [newData.id, newData.filename, newData.type, newData.size],
-      function (error, results, fields) {
-        pool.release();
-        return results;
-      });
+    await sql.truckOfferEditFile(req.body);
+
     return res.status(200).json({
       success: true,
       status: "success",
-      message: "File information edited sucessfully!",
-      offerId: response[0].insertId
+      message: "File information edited sucessfully!"
     });
   } catch (error) {
-    return res.status(500).json({
-      success: false,
-      message: error
-    });
+    return next(new AppError(error), 500);
   }
 });
 
 exports.truckOfferEmail = catchAsync(async (req, res, next) => {
-  // console.log('req', req.body);
-
   if (Object.keys(req.body).length === 0) {
-    return res.status(400).send('Empty object provided');
+    return next(new AppError("Empty object provided"), 400);
   }
 
   try {
     sendEmail({
-      from: 'Клиент',
-      subject: 'Оферта за покривало',
+      from: "Клиент",
+      subject: "Оферта за покривало",
       to: process.env.USER_EMAIL,
       html: `<h1>Оферта от клиент</h1>`,
       attachments: [
@@ -186,13 +187,13 @@ exports.truckOfferEmail = catchAsync(async (req, res, next) => {
     });
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       message:
-        'Thanks for your messege!\n Soon some of our team will respond you.'
+        "Thanks for your messege!\n Soon some of our team will respond you.",
     });
   } catch (err) {
     return next(
-      new AppError('There was an error sending the email. Try again later!'),
+      new AppError("There was an error sending the email. Try again later!"),
       500
     );
   }
--- controllers/windproofCurtainsController.js
+++ controllers/windproofCurtainsController.js
@@ -1,12 +1,15 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const pool = require('../utils/pool2');
+const sql = require('../utils/sql');
 const sendEmail = require('../utils/emailOffer');
 
 exports.windproofCurtainsPriceOffer = catchAsync(async (req, res, next) => {
   try {
     if (Object.keys(req.body).length === 0 || Object.keys(req.body.values).length === 0) {
-      return res.status(400).send('Empty object provided');
+      return next(
+        new AppError("Empty object provided"),
+        400
+      );
     }
     const {
       width,
@@ -19,141 +22,145 @@ exports.windproofCurtainsPriceOffer = catchAsync(async (req, res, next) => {
       pipe_pocket,
       knobs,
       curtain_have_door
-    } = req?.body?.values
+    } = req?.body?.values;
 
-    let priceThick = 0;
-    let finalPrice = 0;
-    let hardwareTextPrice = 0;
-    let plasticKnobsPrice = 2.5;
-    let metalKnobsPrice = 4.5;
-    let strapPlatesPrice = 12;
-    let pockets = 0.5;
-    let zipPrice = 15;
+    const prices = await sql.allWindProofCurtainsPrices();
 
-    const w = Number(width);
-    const h = Number(height);
-    const t = Number(thick);
-    const e = (Number(edge) * 2) / 100;
+    if (Object.keys(prices).length > 0) {
+      const {
+        price_thick_1,
+        price_thick_2,
+        price_plastic_knobs,
+        price_metal_knobs,
+        price_strap_plates,
+        price_pockets,
+        price_zip,
+        price_knobs,
+        price_curtain
+      } = prices[0];
+      const w = Number(width);
+      const h = Number(height);
+      const e = (Number(edge) * 2) / 100;
+      let priceThick = Number(thick) === 0.8 ? price_thick_1 : price_thick_2;
+      let hardwareTextPrice = 0;
+      let finalPrice = 0;
 
-    if (t === 0.8) {
-      priceThick = 24;
-    } else {
-      priceThick = 20;
+      finalPrice = ((w + e) * (h + e));
+      finalPrice = (finalPrice.toFixed(2) * priceThick).toFixed(2);
+
+      if (hardwareText === 'plastic_knobs') {
+        hardwareTextPrice = (((2 * h) / 0.35) * price_plastic_knobs).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (hardwareText === 'metal_knobs') {
+        hardwareTextPrice = (((2 * h) / 0.35) * price_metal_knobs).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (hardwareText === 'strap_plates') {
+        hardwareTextPrice = ((2 * h) * price_strap_plates).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (hardwareText === 'pockets') {
+        hardwareTextPrice = (((2 * h) / 0.15) * price_pockets).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (zips === true) {
+        hardwareTextPrice = (w * price_zip).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (lower_apron === true) {
+        hardwareTextPrice = ((w * 0.35) * price_strap_plates).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (pipe_pocket === true) {
+        hardwareTextPrice = ((w * 0.20) * price_strap_plates).toFixed(2);
+        finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
+      }
+
+      if (knobs === true) {
+        finalPrice = (Number(finalPrice) + price_knobs).toFixed(2);
+      }
+
+      if (curtain_have_door === true) {
+        finalPrice = (Number(finalPrice) + price_curtain).toFixed(2);
+      }
+
+      res.status(200).json({
+        'status': 'success',
+        'result': finalPrice
+      });
     }
-
-    finalPrice = ((w + e) * (h + e));
-    finalPrice = (finalPrice.toFixed(2) * priceThick).toFixed(2);
-
-    if (hardwareText === 'plastic_knobs') {
-      hardwareTextPrice = (((2 * h) / 0.35) * plasticKnobsPrice).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (hardwareText === 'metal_knobs') {
-      hardwareTextPrice = (((2 * h) / 0.35) * metalKnobsPrice).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (hardwareText === 'strap_plates') {
-      hardwareTextPrice = ((2 * h) * strapPlatesPrice).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (pockets === 'pockets') {
-      hardwareTextPrice = (((2 * h) / 0.15) * pockets).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (zips === true) {
-      hardwareTextPrice = (w * zipPrice).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (lower_apron === true) {
-      hardwareTextPrice = ((w * 0.35) * strapPlatesPrice).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (pipe_pocket === true) {
-      hardwareTextPrice = ((w * 0.20) * strapPlatesPrice).toFixed(2);
-      finalPrice = Number(finalPrice) + Number(hardwareTextPrice);
-    }
-
-    if (knobs === true) {
-      finalPrice = (Number(finalPrice) + 9).toFixed(2);
-    }
-
-    if (curtain_have_door === true) {
-      finalPrice = (Number(finalPrice) + 60).toFixed(2);
-    }
-
-    res.status(200).json({
-      'status': 'success',
-      'result': finalPrice
-    });
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error
-    });
+    return next(
+      new AppError(error),
+      500
+    );
   }
 });
 
 exports.windproofCurtainsOfferFile = catchAsync(async (req, res, next) => {
   try {
     if (Object.keys(req.body).length === 0) {
-      return res.status(400).send('Empty object provided');
+      return next(
+        new AppError("Empty object provided"),
+        400
+      );
     }
 
-    let response = await pool.query("INSERT INTO `windproof_curtains` SET ?", req.body, function (error, results, fields) {
-      pool.release();
-      return results;
-    });
+    const response = await sql.windproofCurtainsOfferSaveFile(req.body);
+   
     return res.status(200).json({
       success: true,
       status: "success",
       message: "File information saved sucessfully!",
-      offerId: response[0].insertId
+      offerId: response
     });
   } catch (error) {
-    return res.status(400).json({
-      success: false,
-      message: error
-    });
+    return next(
+      new AppError(error),
+      500
+    );
   }
 });
 
 exports.windproofCurtainsComparedFiles = catchAsync(async (req, res, next) => {
   try {
-    const newData = req.body;
+    const { id, filename, type, size } = req.body;
 
-    if (!newData.id || !newData.filename || !newData.type || !newData.size) {
-      return res.status(400).send('Missing required parameters');
+    if (!id || !filename || !type || !size) {
+      return next(
+        new AppError("Missing required parameters"),
+        400
+      );
     }
 
-    let response = await pool.query('UPDATE windproof_curtains SET filename = ?,type = ?,size = ? WHERE id=?', [newData.id, newData.filename, newData.type, newData.size],
-      function (error, results, fields) {
-        pool.release();
-        return results;
-      });
+    await sql.windproofCurtainsOfferEditFile(req.body);
+    
     return res.status(200).json({
       success: true,
       status: "success",
-      message: "File information edited sucessfully!",
-      offerId: response[0].insertId
+      message: "File information edited sucessfully!"
     });
   } catch (error) {
-    console.log('error', error);
-    return res.status(500).json({
-      success: false,
-      message: error
-    });
+    return next(
+      new AppError(error),
+      500
+    );
   }
 });
 
 exports.windproofCurtainsOfferEmail = catchAsync(async (req, res, next) => {
   if (Object.keys(req.body).length === 0) {
-    return res.status(400).send('Empty object provided');
+    return next(
+      new AppError("Empty object provided"),
+      400
+    );
   }
 
   try {
