5f806fb9f691a4bbf5435d39a32c5dda63e7cc9e
Santiya Georgieva <santiya.georgigeva@gmail.com>
2024-04-05 19:53:46 +0300

    restrict the response to not return the id property

--- utils/iterateEntries.js
+++ utils/iterateEntries.js
@@ -1,6 +1,10 @@
 module.exports = iterateEntries = (node, fn) => {
   const newNode = {};
-  Object.entries(node).forEach(([key, val]) => (newNode[key] = fn(val)));
+  Object.entries(node).forEach(([key, val]) => {
+    if (key !== 'id') {
+      return newNode[key] = fn(val);
+    }
+  });
 
   return newNode;
 }
