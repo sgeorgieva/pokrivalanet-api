e0df97380446cafd12bd0d75ef2a82be9c42f0d7
Santiya Georgieva <santiyageorgeiva@yahoo.com>
2024-09-04 16:57:37 +0300

    update server configuration

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const pc = require("picocolors");
 dotenv.config({ path: "./config.env" });
 
 // START THE SERVER
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 const server = app
   .listen(port, "0.0.0.0", () => {
